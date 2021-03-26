9db06c9ad7468e063c74c63f0a964b0efcc5254b
sergey <sashumihin@gmail.com>
2021-03-26 14:19:11 +0300

    1

--- app/src/main/java/com/shumikhin/juncalc/MainActivity.java
+++ app/src/main/java/com/shumikhin/juncalc/MainActivity.java
@@ -1,14 +1,21 @@
 package com.shumikhin.juncalc;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+//1. Напишите обработку каждой кнопки из макета калькулятора.
+//2. Создайте объект с данными и операциями калькулятора. Продумайте, каким образом будете хранить введённые пользователем данные.
+//3. * Создайте макет калькулятора для горизонтальной ориентации экрана и отображайте его в ландшафтной ориентации.
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
     }
 }
